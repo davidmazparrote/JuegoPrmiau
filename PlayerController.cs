55cd724ba993b1b119e77d4c229175178c893ae1
davidmazparrote <43190734+davidmazparrote@users.noreply.github.com>
2019-04-01 19:36:20 -0300

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,31 +6,30 @@ public class PlayerController : MonoBehaviour {
 
     public GameObject game;
     private Animator animator;
-    private AudioSource AudioPlayer; //  para los audios del personaje
-    public AudioClip SoundJump, SoundDie, SoundSlide; //los enlazo en el unity en el playerController
+    private AudioSource AudioPlayer; 
+    public AudioClip SoundJump, SoundDie, SoundSlide; 
     public AudioClip SoundPoint;
     public ParticleSystem Trail;
 
-    //variable para importar y usar el gameObject en otra clase...en unity lo arrastro el gamecanvas al script game para poder usarlo
-    // Start is called before the first frame update
+  
 
     void Start () {
 
-        animator = GetComponent<Animator> (); //se detecte automaticamente en el start del player el componente animator se ejecuta este metodo()
-        AudioPlayer = GetComponent<AudioSource> (); //recupero el audio source
+        animator = GetComponent<Animator> (); 
+        AudioPlayer = GetComponent<AudioSource> (); 
     }
 
     // Update is called once per frame
     void Update () {
-        bool GameRun = game.GetComponent<gameController> ().gs == GameState.run; //comprobacion para ver en que estado esta el juego 
-        if ((GameRun && Input.GetKeyDown (KeyCode.UpArrow) || Input.GetMouseButtonDown (0))) //si el estado es run y misma sentencia clik y up para saltar
+        bool GameRun = game.GetComponent<gameController> ().gs == GameState.run; 
+        if ((GameRun && Input.GetKeyDown (KeyCode.UpArrow) || Input.GetMouseButtonDown (0))) 
         {
-            UpdateState ("PlayerJump"); // llamo al mismo metodo, y le cambio el estado. recibe un string con el estado de la animacion que quiero, sino es null.
+            UpdateState ("PlayerJump"); 
 
-            AudioPlayer.clip = SoundJump; //para que no se quede saltando en el animator hago una transicion y lo enlazo con el run para que siga corriendo
+            AudioPlayer.clip = SoundJump; 
             AudioPlayer.Play ();
 
-            //las opciones de duracion de la trancision estan en la animacion- inspector- lopp
+          
         }
         if (GameRun && Input.GetKeyDown (KeyCode.DownArrow) ) {
             UpdateState ("PlayerSlide");
@@ -41,28 +40,28 @@ public class PlayerController : MonoBehaviour {
 
     }
 
-    public void UpdateState (string state = null) //actualizar el estado,este metodo recibe un string state si no le paso ninguno es null por defecto
+    public void UpdateState (string state = null) 
     {
-        if (state != null) { //si el estado es diferente de null
-            animator.Play (state); //el nombre del estado que queremos cambiar
+        if (state != null) { 
+            animator.Play (state);
 
         }
     }
-    public void OnTriggerEnter2D (Collider2D other) //metodo sobrescrito para collisionar
+    public void OnTriggerEnter2D (Collider2D other) 
 
     {
 
-        if (other.gameObject.tag == "Enemy") //el tag con el que colisionamos es un enemy y en el unity en prefabs le cambiamos el tag al enemigo
+        if (other.gameObject.tag == "Enemy") 
         {
             UpdateState ("PlayerDead");
-            game.GetComponent<gameController> ().gs = GameState.die; //dentro de game obtengo el componente de la clase principal,la instancia gs, el estado mas el estado de morir
-            game.SendMessage ("ResetTimeScale", 0.5f); //reseteo el tiempo y el 0.5 es para que se reduzca a la mitad la velocidad al morir
+            game.GetComponent<gameController> ().gs = GameState.die; 
+            game.SendMessage ("ResetTimeScale", 0.5f); 
 
-            game.GetComponent<AudioSource> ().Stop (); //metodo stop para parar la musica cuando muere
+            game.GetComponent<AudioSource> ().Stop (); 
             AudioPlayer.clip = SoundDie;
-            AudioPlayer.Play (); //para reproducirlo una vez
+            AudioPlayer.Play (); 
 
-        } else if (other.gameObject.tag == "Points" && !gameController.pointEnable==false ) {         //si choca con un point incremeta punto...importante cambiarle el tag a points en el unity...tiene que ser distino de false para que sume el punto
+        } else if (other.gameObject.tag == "Points" && !gameController.pointEnable==false ) {        
             game.SendMessage ("IncrementPoints");
             AudioPlayer.clip = SoundPoint;
             AudioPlayer.Play ();
